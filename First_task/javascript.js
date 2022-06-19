71545852cbb62aac900bad7e8001687cb19aeeb0
Nastya Shevchenko <anastasias859@gmail.com>
2022-06-19 12:34:33 +0300

    Initial commit

---
+++ First_task/javascript.js
@@ -0,0 +1,8 @@
+function showMessage() {
+    const userName = prompt('Как тебя зовут?',);
+    const userAge = prompt('Сколько тебе лет?');
+    let userCity = prompt('Где ты живешь?');
+    alert(`Привет, ${userName}! Тебе ${userAge} лет и ты живешь в городе ${userCity}.`);
+}
+
+
---
+++ Second_task_calc/index.js
@@ -0,0 +1,22 @@
+function sum() {
+    let a = prompt('Введите первое число', '');
+    let b = prompt('Введите второе число', '');
+    alert(+a + +b);
+} 
+
+function min() {
+    let a = prompt('Введите первое число', '');
+    let b = prompt('Введите второе число', '');
+    alert(+a - +b);
+}
+function mult() {
+    let a = prompt('Введите первое число', '');
+    let b = prompt('Введите второе число', '');
+    alert(+a * +b);
+}
+function div() {
+    let a = prompt('Введите первое число', '');
+    let b = prompt('Введите второе число', '');
+    alert(+a / +b);
+}
+
