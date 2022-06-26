2fac0bd5ae30bb4e36ed214633b5b2f0adeec378
Nastya Shevchenko <anastasias859@gmail.com>
2022-06-26 18:47:31 +0300

    added tasks to 14th week

--- Second_task_calc/index.js
+++ Second_task_calc/index.js
@@ -1,9 +1,8 @@
-function sum() {
+/*function sum() {
     let a = prompt('Введите первое число', '');
     let b = prompt('Введите второе число', '');
     alert(+a + +b);
 } 
-
 function min() {
     let a = prompt('Введите первое число', '');
     let b = prompt('Введите второе число', '');
@@ -18,5 +17,51 @@ function div() {
     let a = prompt('Введите первое число', '');
     let b = prompt('Введите второе число', '');
     alert(+a / +b);
+}*/
+
+
+function sum() {
+    let number1;
+    let number2;
+
+    number1 = document.getElementById('num1').value;
+    number2 = document.getElementById('num2').value;
+
+    let result = +number1 + +number2;
+    document.getElementById('out').innerHTML = result;
 }
 
+function min() {
+    let number1;
+    let number2;
+
+    number1 = document.getElementById('num1').value;
+    number2 = document.getElementById('num2').value;
+
+    let result = number1 - number2;
+    document.getElementById('out').innerHTML = result;
+}
+
+
+function mult() {
+    let number1;
+    let number2;
+
+    number1 = document.getElementById('num1').value;
+    number2 = document.getElementById('num2').value;
+
+    let result = number1 * number2;
+    document.getElementById('out').innerHTML = result;
+}
+
+
+function div() {
+    let number1;
+    let number2;
+
+    number1 = document.getElementById('num1').value;
+    number2 = document.getElementById('num2').value;
+
+    let result = number1 / number2;
+    document.getElementById('out').innerHTML = result;
+}
